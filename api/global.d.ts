05717cb499511dc04e89746602d1bfafa45556a3
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-09-25 19:31:29 -0500

    scsvbs

--- api/global.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@duneanalytics/client-sdk" {
-    export class DuneClient {
-      constructor(apiKey: string);
-      getLatestResult(params: { queryId: number }): Promise<any>;
-    }
-  }
